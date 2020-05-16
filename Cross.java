725761f778993ef87a6b4d7015d124a5a6bbab8d
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2020-05-16 21:22:19 +0530

    Add files via upload

---
+++ Cross.java
@@ -0,0 +1,11 @@
+class Cross
+{
+  public static void main(String arg[])
+ { 
+    String s1 = "RAHUL";
+    String s2 = "RAJ";
+    System.out.println("First string: "+s1);
+    System.out.println("Second string: "+s2);
+    System.out.println("Concatenate string: "+s1+s2);
+ }
+}
