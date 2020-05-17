1aca5cfe365cf4c05a9c4c26733060abc3222127
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2020-05-17 22:35:06 +0530

    Add files via upload

---
+++ Sweet.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sweet
+{
+  public static void main(String arg[])
+{
+  Scanner x = new Scanner(System.in);
+  System.out.println("Enter a Number: ");
+  int num = x.nextInt();
+  System.out.print("Square of Given Number  "+ num +" = "+(num*num));
+ }
+}
