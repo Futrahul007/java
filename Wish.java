287325c03d15260fc770fb99c49e7b4dfa3dff9a
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2020-05-17 22:34:41 +0530

    Add files via upload

---
+++ Wish.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Wish
+{
+  public static void main(String arg[])
+{
+  Scanner x = new Scanner(System.in);
+ System.out.println("Enter Friend Name ");
+ String str=x.nextLine();
+ System.out.println("HAPPY NEW YEAR :: "+str);
+}
+}
